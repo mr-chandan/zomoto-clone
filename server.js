fddc004cc1cdac15ed7396172ba9d5eb8bd86e13
chandan <chandanchethan321@gmail.com>
2022-06-29 17:23:45 +0530

    making home

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000
 // app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
-
+app.use(express.static('public'))
 
 
 app.get('/', (req, res) => {
@@ -15,8 +15,6 @@ app.get('/', (req, res) => {
     res.render('home')
 
 })
-
-
 
 
 
