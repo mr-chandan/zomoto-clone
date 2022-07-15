df69f100792fd0921355a16aa8fc6755ded8ccbe
chandan <chandanchethan321@gmail.com>
2022-07-15 17:55:42 +0530

    passport

--- routes/web.js
+++ routes/web.js
@@ -1,16 +1,26 @@
 const homecontroller = require('../app/http/controllers/homecontrollers')
 const authcontrollers = require('../app/http/controllers/authcontrollers')
 const cartcontrollers = require('../app/http/controllers/customers/cartconnection')
-
+const guest = require('../app/http/middlewares/guest')
 function initroutes(app){
 
     app.get('/',homecontroller().index)
-    
+
+
+
+
+    app.get('/login',guest,authcontrollers().login)
+    app.post('/login',authcontrollers().postlogin)
+
+
+    app.get('/register', guest,authcontrollers().register)
+    app.post('/register',authcontrollers().postregister)
+
+    app.post('/logout',authcontrollers().logout)
+
+
+
     app.get('/cart',cartcontrollers().cart)
-    
-    app.get('/login',authcontrollers().login)
-    
-    app.get('/register', authcontrollers().register)
     app.post('/update-cart', cartcontrollers().update)
 
 }
--- server.js
+++ server.js
@@ -9,13 +9,14 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoStore = require('connect-mongo')
-
+const passport = require('passport')
 
 
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
 
@@ -29,6 +30,14 @@ app.use(session({
     saveUninitialized: false,
     cookie: { maxAge: 1000 * 60 * 60 * 24 }
 }))
+
+
+const passportinit = require('./app/config/passport')
+passportinit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
+
 app.use((req, res, next) => {
     res.locals.session = req.session;
     res.locals.user = req.user;
@@ -36,6 +45,7 @@ app.use((req, res, next) => {
   });
 
 require('./routes/web')(app)
+
 
 
 app.listen(PORT, () => {
