f569836a553a7ee987bb52affd0f82b33e8ede94
chandan <chandanchethan321@gmail.com>
2022-07-29 18:26:34 +0530

    wqew

--- app/http/controllers/admin/ordercontoller.js
+++ app/http/controllers/admin/ordercontoller.js
@@ -1,6 +1,6 @@
-const order = require("../../../models/order")
+const order = require("../../../models/order");
 
-const Order = require('../../../models/order')
+const Order = require('../../../models/order');
 
 function ordercontroller() {
     return {
